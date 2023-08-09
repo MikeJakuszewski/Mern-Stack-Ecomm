d43a711ac6d59b708cf8bdb3b446452cf8aef3c6
Mike Jakuszewski <michael_jakuszewski@hotmail.com>
2023-08-09 15:10:42 -0400

    added models and database

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import products from "./data/products.js";
 import dotenv from "dotenv";
 dotenv.config();
+import connectDB from "./config/db.js";
 const port = process.env.PORT || 4000;
 
 const app = express();
@@ -21,3 +22,13 @@ app.get("/api/products/:id", (req, res) => {
 app.listen(port, () => {
   console.log(`Server is running on Port ${port}`);
 });
+
+const startDB = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+  } catch (error) {
+    console.log(`Error: ${error}`);
+  }
+};
+
+startDB();
