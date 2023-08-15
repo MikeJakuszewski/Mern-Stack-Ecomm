c63e8e35a9896c747bf4f52dbba3789bbf76dd1c
Mike Jakuszewski <michael_jakuszewski@hotmail.com>
2023-08-15 11:50:20 -0400

    sepereated util functions and add Cart functionality

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -33,7 +33,7 @@ const ProductScreen = () => {
   const addToCartHandler = () => {
     dispatch(addToCart({ ...product, qty }));
     console.log(dispatch(addToCart({ ...product, qty })));
-    // navigate("/cart");
+    navigate("/cart");
   };
 
   return (
