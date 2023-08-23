aeab1e88052e6c777eade1ee6770d2ccbcbfaf7e
Mike Jakuszewski <michael_jakuszewski@hotmail.com>
2023-08-23 15:31:13 -0400

    added creating product functionality

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -3,9 +3,14 @@ const router = express.Router();
 import {
   getProducts,
   getProductById,
+  createProduct,
 } from "../controllers/productController.js";
+import { protect, admin } from "../middleware/authMiddleware.js";
 
 router.get("/", getProducts);
 router.get("/:id", getProductById);
+
+//Admin
+router.post("/", protect, admin, createProduct);
 
 export default router;
