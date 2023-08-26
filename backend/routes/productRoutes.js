fa5facf2b9aa6caa787e041f43a83f3925ee95ac
Mike Jakuszewski <michael_jakuszewski@hotmail.com>
2023-08-26 08:56:54 -0400

    added small fixes

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -10,15 +10,16 @@ import {
   getTopProducts,
 } from "../controllers/productController.js";
 import { protect, admin } from "../middleware/authMiddleware.js";
+import checkObjectId from "../middleware/checkObjectId.js";
 
 router.get("/", getProducts);
 router.get("/top", getTopProducts);
-router.get("/:id", getProductById);
-router.post("/:id/reviews", protect, createProductReview);
+router.get("/:id", checkObjectId, getProductById);
+router.post("/:id/reviews", protect, checkObjectId, createProductReview);
 
 //Admin
 router.post("/", protect, admin, createProduct);
-router.put("/:id", protect, admin, updateProduct);
-router.delete("/:id", protect, admin, deleteProduct);
+router.put("/:id", protect, admin, checkObjectId, updateProduct);
+router.delete("/:id", protect, admin, checkObjectId, deleteProduct);
 
 export default router;
